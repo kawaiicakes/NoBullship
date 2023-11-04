f7b4b988b900c91c009d88581ed8ce5f90c4374f
kawaiicakes <vomitmopper@gmail.com>
2023-11-04 13:07:28 -0600

    New method override delegates to super while also caching a BlockState value. This is much easier to serialize and can be extended to allow comprehensive definition of a BlockState for testing.

--- src/main/java/io/github/kawaiicakes/nobullship/data/MultiblockRecipeBuilder.java
+++ src/main/java/io/github/kawaiicakes/nobullship/data/MultiblockRecipeBuilder.java
@@ -3,16 +3,19 @@ package io.github.kawaiicakes.nobullship.data;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 import net.minecraft.world.level.block.state.pattern.BlockPatternBuilder;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class MultiblockRecipeBuilder extends BlockPatternBuilder {
     protected final ResourceLocation result;
+    protected final Map<String, BlockState> lookupSimple = new HashMap<>();
 
     protected MultiblockRecipeBuilder(ResourceLocation result) {
         super();
@@ -28,7 +31,7 @@ public class MultiblockRecipeBuilder extends BlockPatternBuilder {
     }
 
     public void save(Consumer<FinishedMultiblockRecipe> consumer, ResourceLocation id) {
-        consumer.accept(new Result(id, this.result, this.pattern, this.lookup, this.height, this.width));
+        consumer.accept(new Result(id, this.result, this.pattern, this.lookupSimple, this.height, this.width));
     }
 
     @Override
@@ -36,20 +39,23 @@ public class MultiblockRecipeBuilder extends BlockPatternBuilder {
         return (MultiblockRecipeBuilder) super.aisle(pAisle);
     }
 
-    @Override
-    public MultiblockRecipeBuilder where(char pSymbol, Predicate<BlockInWorld> pBlockMatcher) {
-        return (MultiblockRecipeBuilder) super.where(pSymbol, pBlockMatcher);
+    /**
+     * This method must be used if a proper lookup key mapping is to be generated.
+     */
+    public MultiblockRecipeBuilder where(char pSymbol, BlockState state) {
+        this.lookupSimple.put(String.valueOf(pSymbol), state);
+        return (MultiblockRecipeBuilder) this.where(pSymbol, BlockInWorld.hasState(testState -> Objects.equals(testState, state)));
     }
 
     public static class Result implements FinishedMultiblockRecipe {
         protected final ResourceLocation id;
         protected final ResourceLocation result;
         protected final List<String[]> recipe;
-        protected final Map<Character, Predicate<BlockInWorld>> lookup;
+        protected final Map<String, BlockState> lookup;
         protected final int height;
         protected final int width;
 
-        public Result(ResourceLocation id, ResourceLocation result, List<String[]> recipe, Map<Character, Predicate<BlockInWorld>> lookup, int height, int width) {
+        public Result(ResourceLocation id, ResourceLocation result, List<String[]> recipe, Map<String, BlockState> lookup, int height, int width) {
             this.id = id;
             this.result = result;
             this.recipe = recipe;
@@ -62,10 +68,11 @@ public class MultiblockRecipeBuilder extends BlockPatternBuilder {
         public void serializeRecipeData(JsonObject pJson) {
             // TODO - add conditional serialization stuff
             JsonObject keyMappings = new JsonObject();
-            for (Map.Entry<Character, Predicate<BlockInWorld>> entry : this.lookup.entrySet()) {
+            for (Map.Entry<String, BlockState> entry : this.lookup.entrySet()) {
+                if (entry.getKey().equals(" ")) continue;
                 JsonObject mapping = new JsonObject();
                 // TODO - figure out how tf to serialize Predicate<BlockInWorld> then continue this
-                mapping.addProperty();
+                mapping.addProperty("blockstate", entry.getValue().toString());
                 keyMappings.add(String.valueOf(entry.getKey()), mapping);
             }
 
@@ -80,8 +87,6 @@ public class MultiblockRecipeBuilder extends BlockPatternBuilder {
 
             pJson.add("key", keyMappings);
             pJson.add("recipe", recipePattern);
-            pJson.addProperty("height", this.height);
-            pJson.addProperty("width", this.width);
             pJson.addProperty("result", this.result.toString());
         }
 
