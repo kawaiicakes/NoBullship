417d2ab915d769f802bb22137dd036b1a008e129
kawaiicakes <vomitmopper@gmail.com>
2023-11-03 21:17:23 -0600

    Provider now reliant on new FinishedMultiblockRecipe interface.

---
+++ src/main/java/io/github/kawaiicakes/nobullship/data/FinishedMultiblockRecipe.java
@@ -0,0 +1,22 @@
+package io.github.kawaiicakes.nobullship.data;
+
+import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+
+public interface FinishedMultiblockRecipe {
+    void serializeRecipeData(JsonObject pJson);
+
+    /**
+     * Gets the JSON for the recipe.
+     */
+    default JsonObject serializeRecipe() {
+        JsonObject jsonobject = new JsonObject();
+        this.serializeRecipeData(jsonobject);
+        return jsonobject;
+    };
+
+    /**
+     * Gets the ID for the recipe.
+     */
+    ResourceLocation getId();
+}
--- src/main/java/io/github/kawaiicakes/nobullship/datagen/MultiblockRecipeProvider.java
+++ src/main/java/io/github/kawaiicakes/nobullship/datagen/MultiblockRecipeProvider.java
@@ -1,37 +1,31 @@
 package io.github.kawaiicakes.nobullship.datagen;
 
+import com.google.common.collect.Sets;
 import com.google.gson.JsonObject;
 import com.mojang.logging.LogUtils;
-import io.github.kawaiicakes.nobullship.data.MultiblockRecipe;
+import io.github.kawaiicakes.nobullship.data.FinishedMultiblockRecipe;
+import io.github.kawaiicakes.nobullship.data.MultiblockRecipeBuilder;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+import net.minecraft.world.level.block.state.predicate.BlockStatePredicate;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import static io.github.kawaiicakes.nobullship.NoBullship.MOD_ID;
 import static net.minecraft.data.DataGenerator.Target.DATA_PACK;
 
 public class MultiblockRecipeProvider implements DataProvider {
     private static final Logger LOGGER = LogUtils.getLogger();
-
-    protected static MultiblockRecipe testRecipe() {
-        ArrayList<String[]> array = new ArrayList<>(1);
-        array.add(new String[]{" # ", " # ", "###"});
-        return new MultiblockRecipe(
-                new ResourceLocation(MOD_ID, "balls"),
-                array,
-                new ResourceLocation("creeper")
-        );
-    }
-
     protected final DataGenerator.PathProvider path;
 
     public MultiblockRecipeProvider(DataGenerator pGenerator, ExistingFileHelper fileHelper) {
@@ -39,14 +33,25 @@ public class MultiblockRecipeProvider implements DataProvider {
     }
 
     @Override
-    public void run(CachedOutput pOutput) throws IOException {
-        Set<ResourceLocation> recipeIds = new HashSet<>();
-        saveRecipe(pOutput, testRecipe().serializeRecipeData(), this.path.json(testRecipe().id));
+    public void run(CachedOutput pOutput) {
+        Set<ResourceLocation> set = Sets.newHashSet();
+        buildRecipes((finishedRecipe) -> {
+                if (!set.add(finishedRecipe.getId())) {
+                    throw new IllegalStateException("Duplicate recipe " + finishedRecipe.getId());
+                } else {
+                    saveRecipe(pOutput, finishedRecipe.serializeRecipe(), this.path.json(finishedRecipe.getId()));
+                }
+        });
+
     }
 
     @Override
     public String getName() {
         return MOD_ID + ":" + "multiblock_recipe_provider";
+    }
+
+    protected void buildRecipes(Consumer<FinishedMultiblockRecipe> consumer) {
+        buildCreeper(consumer);
     }
 
     private static void saveRecipe(CachedOutput pOutput, JsonObject pRecipeJson, Path pPath) {
@@ -55,6 +60,15 @@ public class MultiblockRecipeProvider implements DataProvider {
         } catch (IOException ioexception) {
             LOGGER.error("Couldn't save recipe {}", pPath, ioexception);
         }
+    }
 
+    private static void buildCreeper(Consumer<FinishedMultiblockRecipe> consumer) {
+        ((MultiblockRecipeBuilder) MultiblockRecipeBuilder.of(new ResourceLocation("minecraft", "creeper"))
+                .where('#', BlockInWorld.hasState(BlockStatePredicate.forBlock(Blocks.LIME_WOOL)))
+                .aisle(
+                        " # ",
+                        " # ",
+                        "###"
+                )).save(consumer, new ResourceLocation("minecraft", "creeper"));
     }
 }
