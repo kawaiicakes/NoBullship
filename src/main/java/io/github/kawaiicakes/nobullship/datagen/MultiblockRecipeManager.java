2fcfdc478d02761f83f47f05fdb17b5a3f4f66b0
kawaiicakes <vomitmopper@gmail.com>
2023-11-04 13:58:03 -0600

    quick switch to returning a Pair value for the Map as the key will not necessarily indicate the result of the recipe.

--- src/main/java/io/github/kawaiicakes/nobullship/datagen/MultiblockRecipeManager.java
+++ src/main/java/io/github/kawaiicakes/nobullship/datagen/MultiblockRecipeManager.java
@@ -2,6 +2,7 @@ package io.github.kawaiicakes.nobullship.datagen;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.*;
+import com.mojang.datafixers.util.Pair;
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -13,14 +14,17 @@ import net.minecraft.world.level.block.state.pattern.BlockPatternBuilder;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
-import java.util.List;
 import java.util.Map;
 
 public class MultiblockRecipeManager extends SimpleJsonResourceReloadListener {
     private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().disableHtmlEscaping().create();
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private Map<ResourceLocation, BlockPattern> recipes = ImmutableMap.of();
+    /**
+     * A map available on the serverside containing the recipe id as a key, and a <code>Pair</code> of the
+     * <code>BlockPattern</code>, which is the recipe, and the result of the recipe as a <code>ResourceLocation</code>.
+     */
+    private Map<ResourceLocation, Pair<BlockPattern, ResourceLocation>> recipes = ImmutableMap.of();
 
     public MultiblockRecipeManager() {
         super(GSON, "entity_recipes");
@@ -28,15 +32,19 @@ public class MultiblockRecipeManager extends SimpleJsonResourceReloadListener {
 
     @Override
     protected void apply(Map<ResourceLocation, JsonElement> pObject, ResourceManager pResourceManager, ProfilerFiller pProfiler) {
-        ImmutableMap.Builder<ResourceLocation, BlockPattern> builder = ImmutableMap.builder();
+        ImmutableMap.Builder<ResourceLocation, Pair<BlockPattern, ResourceLocation>> builder = ImmutableMap.builder();
 
         for(Map.Entry<ResourceLocation, JsonElement> entry : pObject.entrySet()) {
             ResourceLocation resourcelocation = entry.getKey();
             if (resourcelocation.getPath().startsWith("_")) continue;
 
             try {
-                BlockPattern recipe = fromJson(GsonHelper.convertToJsonObject(entry.getValue(), "top element"));
-                if (recipe == null) continue;
+                Pair<BlockPattern, ResourceLocation> recipe = fromJson(GsonHelper.convertToJsonObject(entry.getValue(), "top element"));
+                if (recipe == null) {
+                    LOGGER.info("Skipping recipe " + resourcelocation + " because it has empty Z arrays!");
+                    continue;
+                }
+
                 builder.put(resourcelocation, recipe);
             } catch (IllegalArgumentException | JsonParseException jsonParseException) {
                 LOGGER.error("Parsing error loading recipe {}", resourcelocation, jsonParseException);
@@ -49,15 +57,17 @@ public class MultiblockRecipeManager extends SimpleJsonResourceReloadListener {
 
     @Nullable
     public BlockPattern checkPattern(ResourceLocation recipeId) {
-        return this.recipes.getOrDefault(recipeId, null);
+        return this.recipes.getOrDefault(recipeId, null).getFirst();
     }
 
     /**
      * Essentially a deserializer.
      */
     @Nullable
-    protected static BlockPattern fromJson(JsonObject json) {
+    protected static Pair<BlockPattern, ResourceLocation> fromJson(JsonObject json) {
         JsonObject jsonRecipe = json.getAsJsonObject("recipe");
+        ResourceLocation result = new ResourceLocation(json.getAsJsonObject("result").getAsString());
+
         BlockPatternBuilder builder = BlockPatternBuilder.start();
 
         for (int i = 0; i < json.size(); i++) {
@@ -65,6 +75,6 @@ public class MultiblockRecipeManager extends SimpleJsonResourceReloadListener {
             if (aisle.isEmpty()) return null;
         }
 
-        return builder.build();
+        return new Pair<>(builder.build(), result);
     }
 }
