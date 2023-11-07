ec06c2ef86f32b65dd6ff7c0399c6356f361bee3
kawaiicakes <vomitmopper@gmail.com>
2023-11-07 14:58:00 -0700

    slots now use caps.

--- src/main/java/io/github/kawaiicakes/nobullship/screen/MultiblockWorkshopMenu.java
+++ src/main/java/io/github/kawaiicakes/nobullship/screen/MultiblockWorkshopMenu.java
@@ -5,10 +5,11 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ResultSlot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.SlotItemHandler;
 
 import java.util.Objects;
 
@@ -30,18 +31,20 @@ public class MultiblockWorkshopMenu extends AbstractContainerMenu {
         this.addPlayerInventory(inventory);
         this.addPlayerHotbar(inventory);
 
-        for(int i = 0; i < 3; ++i) {
-            for(int j = 0; j < 3; ++j) {
-                this.addSlot(new Slot(this.entity, j + i * 3, 97 + j * 18, 30 + i * 18));
+        this.entity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(handler -> {
+            for(int i = 0; i < 3; ++i) {
+                for(int j = 0; j < 3; ++j) {
+                    this.addSlot(new SlotItemHandler(handler, j + i * 3, 97 + j * 18, 30 + i * 18));
+                }
             }
-        }
 
-        for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(this.entity, i + 9, 97 + i * 18, 102));
-        }
+            for (int i = 0; i < 9; ++i) {
+                this.addSlot(new SlotItemHandler(handler, i + 9, 97 + i * 18, 102));
+            }
 
-        this.addSlot(new Slot(this.entity, 18, 169, 48));
-        this.addSlot(new Slot(this.entity, 19, 169, 26));
+            this.addSlot(new SlotItemHandler(handler, 18, 169, 48));
+            this.addSlot(new SlotItemHandler(handler, 19, 169, 26));
+        });
     }
 
     // TODO
