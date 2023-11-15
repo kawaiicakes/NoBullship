a953a5422a61abb31960a7bcc2f7bebab0037a14
kawaiicakes <vomitmopper@gmail.com>
2023-11-14 23:18:08 -0700

    package info

---
+++ src/main/java/io/github/kawaiicakes/nobullship/api/multiblock/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package io.github.kawaiicakes.nobullship.api.multiblock;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
