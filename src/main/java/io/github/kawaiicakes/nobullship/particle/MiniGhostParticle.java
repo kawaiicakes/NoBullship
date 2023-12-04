778d7ba5cd8534bac83fc6839cd1974529c75662
kawaiicakes <vomitmopper@gmail.com>
2023-12-03 21:46:27 -0700

    nested MiniGhostParticleProvider and added dist markers

--- src/main/java/io/github/kawaiicakes/nobullship/particle/MiniGhostParticle.java
+++ src/main/java/io/github/kawaiicakes/nobullship/particle/MiniGhostParticle.java
@@ -4,8 +4,13 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 
+@OnlyIn(Dist.CLIENT)
 public class MiniGhostParticle extends Particle {
     protected MiniGhostParticle(ClientLevel pLevel, double pX, double pY, double pZ) {
         super(pLevel, pX, pY, pZ);
@@ -19,5 +24,14 @@ public class MiniGhostParticle extends Particle {
     @Override
     public ParticleRenderType getRenderType() {
         return null;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static class MiniGhostParticleProvider implements ParticleProvider<MiniGhostParticleOption> {
+        @Nullable
+        @Override
+        public Particle createParticle(MiniGhostParticleOption pType, ClientLevel pLevel, double pX, double pY, double pZ, double pXSpeed, double pYSpeed, double pZSpeed) {
+            return null;
+        }
     }
 }
