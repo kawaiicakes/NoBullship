7e2ad744d66e4b2f88112aafab4e75cd8cda7e33
kawaiicakes <vomitmopper@gmail.com>
2023-12-10 01:47:02 -0700

    wildcard preview rendering problem fix again

--- src/main/java/io/github/kawaiicakes/nobullship/block/WildcardBlock.java
+++ src/main/java/io/github/kawaiicakes/nobullship/block/WildcardBlock.java
@@ -1,34 +1,52 @@
 package io.github.kawaiicakes.nobullship.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING;
 
 public class WildcardBlock extends Block {
     public WildcardBlock() {
-        super(BlockBehaviour.Properties.of(Material.STONE)
-                .noLootTable()
+        super(BlockBehaviour.Properties.of(Material.GLASS)
+                .sound(SoundType.GLASS)
                 .noOcclusion()
+                .isViewBlocking((x,y,z) -> false)
+                .isSuffocating((x,y,z) -> false)
+                .noLootTable()
                 .explosionResistance(3600000.0F)
                 .strength(-1.0F)
         );
     }
 
     @SuppressWarnings("deprecation")
-    @Override
-    public RenderShape getRenderShape(BlockState pState) {
-        return RenderShape.MODEL;
+    public boolean skipRendering(BlockState pState, BlockState pAdjacentBlockState, Direction pSide) {
+        return pAdjacentBlockState.is(this) || super.skipRendering(pState, pAdjacentBlockState, pSide);
+    }
+
+    @SuppressWarnings("deprecation")
+    public VoxelShape getVisualShape(BlockState pState, BlockGetter pReader, BlockPos pPos, CollisionContext pContext) {
+        return Shapes.empty();
+    }
+
+    @SuppressWarnings("deprecation")
+    public float getShadeBrightness(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return 1.0F;
+    }
+
+    public boolean propagatesSkylightDown(BlockState pState, BlockGetter pReader, BlockPos pPos) {
+        return true;
     }
 
     @Nullable
