7edc4ba4694ac2dc6fd1b62e3b8dc0506aced716
kawaiicakes <vomitmopper@gmail.com>
2023-12-26 15:41:41 -0700

    updates to blockstate properties

--- src/main/java/io/github/kawaiicakes/nobullship/block/BeamBlock.java
+++ src/main/java/io/github/kawaiicakes/nobullship/block/BeamBlock.java
@@ -2,9 +2,13 @@ package io.github.kawaiicakes.nobullship.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,6 +20,8 @@ import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
 
+import static io.github.kawaiicakes.nobullship.Registry.METAL_BEAM_BLOCK;
+
 // TODO: continue overriding and implementing methods
 public class BeamBlock extends Block implements SimpleWaterloggedBlock {
     public static final EnumProperty<Direction.Axis> HORIZONTAL_AXIS = EnumProperty.create(
@@ -25,12 +31,10 @@ public class BeamBlock extends Block implements SimpleWaterloggedBlock {
             Direction.Axis.Z
     );
     public static final BooleanProperty VERTICAL = BooleanProperty.create("vertical");
-    public static final BooleanProperty UP = BlockStateProperties.UP;
-    public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
-    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
-    public static final BooleanProperty EAST = BlockStateProperties.EAST;
-    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
-    public static final BooleanProperty WEST = BlockStateProperties.WEST;
+    public static final EnumProperty<BeamConnection> UP = EnumProperty.create("up", BeamConnection.class);
+    public static final EnumProperty<BeamConnection> DOWN = EnumProperty.create("down", BeamConnection.class);
+    public static final BooleanProperty LEFT = BooleanProperty.create("left");
+    public static final BooleanProperty RIGHT = BooleanProperty.create("right");
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public BeamBlock(Properties properties) {
@@ -38,14 +42,18 @@ public class BeamBlock extends Block implements SimpleWaterloggedBlock {
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(HORIZONTAL_AXIS, Direction.Axis.X)
                 .setValue(VERTICAL, Boolean.FALSE)
-                .setValue(UP, Boolean.FALSE)
-                .setValue(DOWN, Boolean.FALSE)
-                .setValue(NORTH, Boolean.FALSE)
-                .setValue(EAST, Boolean.FALSE)
-                .setValue(SOUTH, Boolean.FALSE)
-                .setValue(WEST, Boolean.FALSE)
+                .setValue(UP, BeamConnection.NONE)
+                .setValue(DOWN, BeamConnection.NONE)
+                .setValue(LEFT, Boolean.FALSE)
+                .setValue(RIGHT, Boolean.FALSE)
                 .setValue(WATERLOGGED, Boolean.FALSE)
         );
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return super.mirror(pState, pMirror);
     }
 
     @SuppressWarnings("deprecation")
@@ -61,12 +69,46 @@ public class BeamBlock extends Block implements SimpleWaterloggedBlock {
 
         boolean isVertical = pContext.getClickedFace().getAxis().equals(Direction.Axis.Y);
 
-        boolean isInWater = pContext.getLevel().getFluidState(pContext.getClickedPos()).getType() == Fluids.WATER;
+        Level level = pContext.getLevel();
+        BlockPos placementPos = pContext.getClickedPos();
+
+        BlockState blockAbove = level.getBlockState(getAbove(placementPos, axisDirection, isVertical));
+        BlockState blockBelow = level.getBlockState(getBelow(placementPos, axisDirection, isVertical));
+        BlockState blockOnLeft = level.getBlockState(getLeft(placementPos, axisDirection));
+        BlockState blockOnRight = level.getBlockState(getRight(placementPos, axisDirection));
+
+        BeamConnection connectionAbove = BeamConnection.NONE;
+        BeamConnection connectionBelow = BeamConnection.NONE;
+
+        if (blockAbove.is(METAL_BEAM_BLOCK.get()) && blockAbove.getValue(VERTICAL) != isVertical) {
+            connectionAbove = axisDirection.equals(blockAbove.getValue(HORIZONTAL_AXIS)) ? BeamConnection.PARALLEL : BeamConnection.PERPENDICULAR;
+        }
+
+        if (blockBelow.is(METAL_BEAM_BLOCK.get()) && blockBelow.getValue(VERTICAL) != isVertical) {
+            connectionBelow = axisDirection.equals(blockBelow.getValue(HORIZONTAL_AXIS)) ? BeamConnection.PARALLEL : BeamConnection.PERPENDICULAR;
+        }
+
+        boolean attachesOnLeft = blockOnLeft.is(METAL_BEAM_BLOCK.get()) && !blockOnLeft.getValue(HORIZONTAL_AXIS).equals(axisDirection);
+        boolean attachesOnRight = blockOnRight.is(METAL_BEAM_BLOCK.get()) && !blockOnRight.getValue(HORIZONTAL_AXIS).equals(axisDirection);
+
+        boolean isInWater = pContext.getLevel().getFluidState(placementPos).getType() == Fluids.WATER;
 
         return this.defaultBlockState()
                 .setValue(HORIZONTAL_AXIS, axisDirection)
                 .setValue(VERTICAL, isVertical)
+                .setValue(UP, connectionAbove)
+                .setValue(DOWN, connectionBelow)
+                .setValue(LEFT, attachesOnLeft)
+                .setValue(RIGHT, attachesOnRight)
                 .setValue(WATERLOGGED, isInWater);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState updateShape(BlockState pState, Direction pDirection, BlockState pNeighborState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pNeighborPos) {
+        if (!pNeighborState.is(METAL_BEAM_BLOCK.get())) return pState;
+
+        return super.updateShape(pState, pDirection, pNeighborState, pLevel, pCurrentPos, pNeighborPos);
     }
 
     @SuppressWarnings("deprecation")
@@ -82,6 +124,63 @@ public class BeamBlock extends Block implements SimpleWaterloggedBlock {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(HORIZONTAL_AXIS, VERTICAL, UP, DOWN, NORTH, EAST, SOUTH, WEST, WATERLOGGED);
+        pBuilder.add(HORIZONTAL_AXIS, VERTICAL, LEFT, RIGHT, WATERLOGGED);
+    }
+
+    public static BlockPos getLeft(BlockPos pos, Direction.Axis beamAxis) {
+        return switch (beamAxis) {
+            case X -> pos.north();
+            case Z -> pos.east();
+            default -> throw new IllegalArgumentException("Horizontal axes only!");
+        };
+    }
+
+    public static BlockPos getRight(BlockPos pos, Direction.Axis beamAxis) {
+        return switch (beamAxis) {
+            case X -> pos.south();
+            case Z -> pos.west();
+            default -> throw new IllegalArgumentException("Horizontal axes only!");
+        };
+    }
+
+    public static BlockPos getAbove(BlockPos pos, Direction.Axis beamAxis, boolean isVertical) {
+        if (isVertical) {
+            return switch (beamAxis) {
+                case X -> pos.east();
+                case Z -> pos.south();
+                default -> throw new IllegalArgumentException("Horizontal axes only!");
+            };
+        } else {
+            return pos.above();
+        }
+    }
+
+    public static BlockPos getBelow(BlockPos pos, Direction.Axis beamAxis, boolean isVertical) {
+        if (isVertical) {
+            return switch (beamAxis) {
+                case X -> pos.west();
+                case Z -> pos.north();
+                default -> throw new IllegalArgumentException("Horizontal axes only!");
+            };
+        } else {
+            return pos.below();
+        }
+    }
+
+    public enum BeamConnection implements StringRepresentable {
+        NONE("none"),
+        PARALLEL("parallel"),
+        PERPENDICULAR("perpendicular");
+
+        private final String name;
+
+        BeamConnection(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getSerializedName() {
+            return this.name;
+        }
     }
 }
